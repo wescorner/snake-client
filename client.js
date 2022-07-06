d5b91c1aa91bc35bf45627dd2e431c14f1545a68
Wesley Corner <wescorner@gmail.com>
2022-07-06 18:22:17 +0000

    program accepts user input and exits with ctrl + c

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function () {
 	conn.on("connect", () => {
 		console.log("Connected!");
 		conn.write("Name: WES");
+		// setInterval(() => conn.write("Move: up"), 50);
 	});
 
 	conn.on("data", (data) => {
--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const connect = require("./client");
 
 console.log("Connecting...");
 connect();
+
+const setupInput = function () {
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding("utf8");
+	stdin.resume();
+	stdin.on("data", handleUserInput);
+	return stdin;
+};
+
+const handleUserInput = function (data) {
+	// process.on("SIGINT", () => process.exit());
+	if (data == "\u0003") process.exit();
+	// process.stdout.write(data);
+	// process.exit();
+};
+
+setupInput();
