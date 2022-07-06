c3fa3c966380bb1a763831923233f654cf59b55e
Wesley Corner <wescorner@gmail.com>
2022-07-06 17:55:28 +0000

    moved connect function into client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function () {
+	const conn = net.createConnection({
+		host: "165.227.47.243",
+		port: "50541",
+	});
+
+	conn.setEncoding("utf8");
+
+	conn.on("data", (data) => {
+		console.log(data);
+	});
+
+	return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-	const conn = net.createConnection({
-		host: "165.227.47.243",
-		port: "50541",
-	});
-
-	conn.setEncoding("utf8");
-
-	conn.on("data", (data) => {
-		console.log(data);
-	});
-
-	return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting...");
 connect();
